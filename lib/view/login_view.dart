efa7acadb80b8b3c386d12d8a5a8de5890bd2145
Azlan-saja <azlansaja19@gmail.com>
2025-07-24 12:35:14 +0700

    p5

--- lib/view/login_view.dart
+++ lib/view/login_view.dart
@@ -6,26 +6,76 @@ class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: EdgeInsetsGeometry.all(18.0),
-          child: Column(
-            children: [
-              // 1. Gambar,
-              // 2. Teks Judul,
-              Text(
-                "Notes App",
-                style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.teal,
-                  fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: EdgeInsetsGeometry.all(18.0),
+            child: Column(
+              children: [
+                // 1. Gambar,
+                Image.asset(
+                  "lib/assets/images/login.png",
+                  width: 300,
                 ),
-              ),
-              // 3. Inputan Username,
-              // 4. Inputan Password,
-              // 5. Tombol Login,
-              // 6. Teks Buat Akun,
-            ],
+                // 2. Teks Judul,
+                const SizedBox(height: 12),
+                Text(
+                  "Notes App",
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Theme.of(context).colorScheme.primary,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                // 3. Inputan Username,
+                const SizedBox(height: 12),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Username",
+                    prefixIcon: Icon(Icons.person),
+                    fillColor: Colors.grey.shade300,
+                    filled: true,
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.only(top: 13),
+                  ),
+                ),
+                // 4. Inputan Password,
+                const SizedBox(height: 12),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Password",
+                    prefixIcon: Icon(Icons.key),
+                    suffixIcon: Icon(Icons.visibility),
+                    fillColor: Colors.grey.shade300,
+                    filled: true,
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.only(top: 13),
+                  ),
+                ),
+                // 5. Tombol Login,
+                const SizedBox(height: 14),
+                SizedBox(
+                  width: double.infinity,
+                  child: FilledButton.icon(
+                    onPressed: () {},
+                    label: Text("Login"),
+                    icon: Icon(Icons.login),
+                  ),
+                ),
+                // 6. Teks Buat Akun,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("Don't have an account?"),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text("Create account"),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
