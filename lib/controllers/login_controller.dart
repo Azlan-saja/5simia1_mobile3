fb40bd681045a9e0dd97887a2f87c118ebd2df09
Azlan-saja <azlansaja19@gmail.com>
2025-09-11 12:36:37 +0700

    p9

--- lib/controllers/login_controller.dart
+++ lib/controllers/login_controller.dart
@@ -4,8 +4,20 @@ class LoginController {
   final usernameController = TextEditingController();
   final passwordController = TextEditingController();
   bool isVisible = false;
+  final formKey = GlobalKey<FormState>();
 
   tampilkanPassword() {
     isVisible = !isVisible;
+  }
+
+  String? cekValidasi(String? value) {
+    if (value!.isEmpty) {
+      return 'Tidak boleh kosong!';
+    }
+    return null;
+  }
+
+  prosesLogin() {
+    if (formKey.currentState!.validate()) {}
   }
 }
--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -18,84 +18,91 @@ class _LoginViewState extends State<LoginView> {
         child: SafeArea(
           child: Padding(
             padding: EdgeInsetsGeometry.all(18.0),
-            child: Column(
-              children: [
-                // 1. Gambar,
-                Image.asset(
-                  "lib/assets/images/login.png",
-                  width: 300,
-                ),
-                // 2. Teks Judul,
-                const SizedBox(height: 12),
-                Text(
-                  "Notes App",
-                  style: TextStyle(
-                    fontSize: 24,
-                    color: Theme.of(context).colorScheme.primary,
-                    fontWeight: FontWeight.bold,
+            child: Form(
+              key: loginController.formKey,
+              child: Column(
+                children: [
+                  // 1. Gambar,
+                  Image.asset(
+                    "lib/assets/images/login.png",
+                    width: 300,
                   ),
-                ),
-                // 3. Inputan Username,
-                const SizedBox(height: 12),
-                TextFormField(
-                  controller: loginController.usernameController,
-                  decoration: InputDecoration(
-                    hintText: "Username",
-                    prefixIcon: Icon(Icons.person),
-                    fillColor: Colors.grey.shade300,
-                    filled: true,
-                    border: InputBorder.none,
-                    contentPadding: EdgeInsets.only(top: 13),
+                  // 2. Teks Judul,
+                  const SizedBox(height: 12),
+                  Text(
+                    "Notes App",
+                    style: TextStyle(
+                      fontSize: 24,
+                      color: Theme.of(context).colorScheme.primary,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                // 4. Inputan Password,
-                const SizedBox(height: 12),
-                TextFormField(
-                  controller: loginController.passwordController,
-                  obscureText: !loginController.isVisible,
-                  decoration: InputDecoration(
-                    hintText: "Password",
-                    prefixIcon: Icon(Icons.key),
-                    suffixIcon: InkWell(
-                      onTap: () {
-                        setState(() {
-                          loginController.tampilkanPassword();
-                        });
-                      },
-                      child: Icon(
-                        loginController.isVisible
-                            ? Icons.visibility_off
-                            : Icons.visibility,
+                  // 3. Inputan Username,
+                  const SizedBox(height: 12),
+                  TextFormField(
+                    validator: (value) => loginController.cekValidasi(value),
+                    controller: loginController.usernameController,
+                    decoration: InputDecoration(
+                      hintText: "Username",
+                      prefixIcon: Icon(Icons.person),
+                      fillColor: Colors.grey.shade300,
+                      filled: true,
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.only(top: 13),
+                    ),
+                  ),
+                  // 4. Inputan Password,
+                  const SizedBox(height: 12),
+                  TextFormField(
+                    validator: (value) => loginController.cekValidasi(value),
+                    controller: loginController.passwordController,
+                    obscureText: !loginController.isVisible,
+                    decoration: InputDecoration(
+                      hintText: "Password",
+                      prefixIcon: Icon(Icons.key),
+                      suffixIcon: InkWell(
+                        onTap: () {
+                          setState(() {
+                            loginController.tampilkanPassword();
+                          });
+                        },
+                        child: Icon(
+                          loginController.isVisible
+                              ? Icons.visibility_off
+                              : Icons.visibility,
+                        ),
                       ),
+                      fillColor: Colors.grey.shade300,
+                      filled: true,
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.only(top: 13),
                     ),
-                    fillColor: Colors.grey.shade300,
-                    filled: true,
-                    border: InputBorder.none,
-                    contentPadding: EdgeInsets.only(top: 13),
                   ),
-                ),
-                // 5. Tombol Login,
-                const SizedBox(height: 14),
-                SizedBox(
-                  width: double.infinity,
-                  child: FilledButton.icon(
-                    onPressed: () {},
-                    label: Text("Login"),
-                    icon: Icon(Icons.login),
-                  ),
-                ),
-                // 6. Teks Buat Akun,
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text("Don't have an account?"),
-                    TextButton(
-                      onPressed: () {},
-                      child: const Text("Create account"),
+                  // 5. Tombol Login,
+                  const SizedBox(height: 14),
+                  SizedBox(
+                    width: double.infinity,
+                    child: FilledButton.icon(
+                      onPressed: () {
+                        loginController.prosesLogin();
+                      },
+                      label: Text("Login"),
+                      icon: Icon(Icons.login),
                     ),
-                  ],
-                ),
-              ],
+                  ),
+                  // 6. Teks Buat Akun,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text("Don't have an account?"),
+                      TextButton(
+                        onPressed: () {},
+                        child: const Text("Create account"),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
